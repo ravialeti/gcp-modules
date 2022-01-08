d68f1e20c0b77473233b81fcba3e8ac12daddc31
Ravi Aleti <70480130+ravialeti@users.noreply.github.com>
2022-01-08 16:22:04 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,7 +1,14 @@
-network_interface {
-     network = "default"
-     access_config {
-     }
-   } 
-   tags = ["apps1"]
- }
+resource "google_compute_network" "vpc" {
+  name                    = my-vpc
+  description             = "vpc for wordpress instances"
+  routing_mode            = "REGIONAL"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "private" {
+  name          = "private"
+  ip_cidr_range = "192.168.24.0/24"
+  region        = "us-east1" 
+  network       = google_compute_network.vpc.self_link
+  private_ip_google_access = true
+}
