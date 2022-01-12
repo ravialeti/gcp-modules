47696f16920d02e2af463a86782d985a5d78ba13
Aleti <ravialeti00@gmail.com>
2022-01-12 16:34:59 +0530

    deploy

--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -2,7 +2,7 @@ output "vm_name" {
     value = google_compute_instance.wp-vm.name
 }
 
-output "network_ip" {
-    value = google_compute_instance.wp-vm.network_ip
+output "nat_ip" {
+    value = google_compute_instance.wp-vm.nat_ip
 }
 
