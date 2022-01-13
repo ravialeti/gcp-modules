ce5c15e4ed47c74385bfa9e9623414a552a857d5
Aleti <ravialeti00@gmail.com>
2022-01-13 19:45:26 +0530

    deploy

--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -1,7 +1,25 @@
 variable "instance_name" {
    description = "GCP Zone for vm"
   }
-
+variable "vm_type" {
+   description = "VM Machine type"
+  }
+variable "vm_zone" {
+   description = "GCP Zone for vm"
+  }
+variable "tags" {
+   type        = list
+   description = "Network tags used for vm and subnets"
+  }
+variable "vm_image" {
+   description = "VM Machine type"
+  }
+variable "vm_size" {
+   description = "VM size GB"
+  }
+variable "subnet_name" {
+   description = "Subnet name from vpc module"
+}
 
 
 
