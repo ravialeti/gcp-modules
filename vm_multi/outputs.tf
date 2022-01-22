d12aab9035923ec17f40bf77473ec660f35fc059
Aleti <ravialeti00@gmail.com>
2022-01-22 15:43:57 +0530

    depsadfloy

--- vm_multi/outputs.tf
+++ vm_multi/outputs.tf
@@ -1,14 +1,12 @@
 output "vm_name" {
     value = google_compute_instance.wp-vm.*
-    sensitive = true
-}
+    }
 
 output "internal_ip" {
     value = google_compute_instance.wp-vm[0].network_interface[0].network_ip
 }
 
 output "gce_machines" {
-    value = google_compute_instance.wp-vm.*  # Here * indicates that there are more than one arn as we used count as 4   
-sensitive = true
+    value = google_compute_instance.wp-vm.* 
 }
  
