e4ab0fea6180b320968bac071101627f3778da7a
Aleti <ravialeti00@gmail.com>
2022-01-22 16:19:28 +0530

    deploy

--- vm_multi/main.tf
+++ vm_multi/main.tf
@@ -4,7 +4,7 @@ resource "google_compute_instance" "wp-vm" {
   name = "vm001-${count.index}"
   machine_type  = var.vm_type
   zone          = var.vm_zone
-  metadata_startup_script = templatefile("init.sh",
+  metadata_startup_script = templatefile("init.sh")
   boot_disk {
     initialize_params {
      image = var.vm_image
